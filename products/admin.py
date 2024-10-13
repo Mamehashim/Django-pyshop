bd53299d4f28054a032729820ed85e305dff38d4
Mamehashim <mamehshim94@gmail.com>
2024-10-13 05:43:22 -0700

    first commit

---
+++ products/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from . models import Product
+from . models import Offer
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display=('name','price','stock')
+
+class OfferAdmin(admin.ModelAdmin):
+    list_offer=('code','discount') 
+
+admin.site.register(Product,ProductAdmin)
+admin.site.register(Offer,OfferAdmin)
